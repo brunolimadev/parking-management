a362bc045fc5f0d18b8af38da18446bb311a29c5
brunolimadev <bruno.lima.dev@outlook.com>
2024-01-05 19:37:19 -0300

    chore: initial config

---
+++ src/test/java/br/com/fiap/parkingmanagement/ParkingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.parkingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
