9730e2f7d42e8f85e44285704e4e9395f38183fe
brunolimadev <bruno.lima.dev@outlook.com>
2024-01-07 13:26:28 -0300

    chore(msg): Changed validation message

--- src/main/java/br/com/fiap/parkingmanagement/model/dto/UserDto.java
+++ src/main/java/br/com/fiap/parkingmanagement/model/dto/UserDto.java
@@ -10,7 +10,7 @@ public record UserDto(
     @Email(message = "Deve ser um email válido.")
     String email,
     @Size(min = 6, max = 12, message = "Senha de ter no mínimo 6 caracteres.")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$", message = "Password must contain at least one digit, one lowercase, one uppercase, one special character")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$", message = "Senha deve conter pelo menos uma letra maiúscula, uma minúscula, um número e um caractere especial.")
     String password,
     LocalDateTime createdAt,
     UserRoleEnum role
