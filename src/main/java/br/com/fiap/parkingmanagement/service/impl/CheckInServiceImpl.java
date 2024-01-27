e8080fe5ab4a0452805312e18c4ca05ab2ce1230
Wiliam Nascimento <wilnasc990s3@gmail.com>
2024-01-27 14:41:09 -0300

    Adicionando lógica de checking e depÃendência do Gson (para visualizar o que foi salvo no mongo formatado)

--- src/main/java/br/com/fiap/parkingmanagement/service/impl/CheckInServiceImpl.java
+++ src/main/java/br/com/fiap/parkingmanagement/service/impl/CheckInServiceImpl.java
@@ -1,5 +1,6 @@
 package br.com.fiap.parkingmanagement.service.impl;
 
+import br.com.fiap.parkingmanagement.model.dto.ZoneDto;
 import br.com.fiap.parkingmanagement.model.dto.checkin.ParkingTicketDto;
 import br.com.fiap.parkingmanagement.model.entity.*;
 import br.com.fiap.parkingmanagement.model.entity.checkin.*;
@@ -7,33 +8,66 @@ import br.com.fiap.parkingmanagement.model.entity.checkin.Vehicle;
 import br.com.fiap.parkingmanagement.model.entity.checkin.Zone;
 import br.com.fiap.parkingmanagement.repository.CheckInRepository;
 import br.com.fiap.parkingmanagement.service.CheckInService;
+import br.com.fiap.parkingmanagement.service.ZoneService;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CheckInServiceImpl implements CheckInService {
     @Autowired
     private CheckInRepository checkInRepository;
 
+    @Autowired
+    private ZoneService zoneService;
+
     @Override
     public void save (ParkingTicketDto parkingTicketDto, User user) {
         ParkingTicket response = this.checkInRepository.save(assembleParkingTicketEntity(parkingTicketDto));
-        System.out.println(response.toString());
+        System.out.println(new Gson().toJson(response));
     }
 
     private ParkingTicket assembleParkingTicketEntity(ParkingTicketDto parkingTicketDto) {
+        getPriceZonePerHour(parkingTicketDto);
         return new ParkingTicket(
                 parkingTicketDto.period(),
                 LocalDateTime.now().toString(),
-                LocalDateTime.now().plusHours(2L).toString(),
-                new Zone(parkingTicketDto.address().id(), parkingTicketDto.address().local(), "70.00"),
+                LocalDateTime.now().plusHours(Long.parseLong(parkingTicketDto.period())).toString(),
+                new Zone(parkingTicketDto.address().id(), parkingTicketDto.address().local(), String.valueOf(getPriceZonePerHour(parkingTicketDto))),
                 new Vehicle(parkingTicketDto.vehicle().type().name(), parkingTicketDto.vehicle().plate()),
                 new Payment(
                         parkingTicketDto.payment().type(),
                         new Card(parkingTicketDto.payment().card().type(), parkingTicketDto.payment().card().number(), parkingTicketDto.payment().card().verificationCode()),
-                        "100")
+                        String.valueOf(calculatePaymentValue(parkingTicketDto)))
                 );
+    }
+
+    private Double getPriceZonePerHour(ParkingTicketDto parkingTicketDto) {
+        List<ZoneDto> zoneDtoList = zoneService.findByLocal(Optional.ofNullable(parkingTicketDto.address().local()), Optional.<String>empty());
+
+        Optional<ZoneDto> zoneDto = zoneDtoList
+                .stream()
+                .filter(zone -> zone.id() == Long.parseLong(parkingTicketDto.address().id()))
+                .findFirst();
+
+        DayOfWeek day = LocalDateTime.now().getDayOfWeek();
+
+        if (day == DayOfWeek.SATURDAY) {
+            return zoneDto.get().r1SaturdayValuePerHour();
+        }
+        if (day == DayOfWeek.SUNDAY) {
+            return zoneDto.get().r1SundayValuePerHour();
+        }
+
+        return zoneDto.get().r1WeekValuePerHour();
+    }
+
+    private Double calculatePaymentValue(ParkingTicketDto parkingTicketDto) {
+        return Integer.parseInt(parkingTicketDto.period()) * getPriceZonePerHour(parkingTicketDto);
     }
 }
